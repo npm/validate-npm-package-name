b1c5b0ac4125bb47a4be5534f8586f3a5fed1f37
Zeke Sikelianos <zeke@sikelianos.com>
2014-12-17 22:48:17 -0800

    s/valid/validate/ in tests

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-var valid = require("..")
+var validate = require("..")
 var test = require("tap").test
 var path = require("path")
 var fs = require("fs")
@@ -7,75 +7,75 @@ test("validate-npm-package-name", function (t) {
 
   // Traditional
 
-  t.deepEqual(valid("some-package"), {valid: true})
-  t.deepEqual(valid("example.com"), {valid: true})
-  t.deepEqual(valid("under_score"), {valid: true})
-  t.deepEqual(valid("period.js"), {valid: true})
-  t.deepEqual(valid("123numeric"), {valid: true})
-  t.deepEqual(valid("crazy!"), {valid: true})
+  t.deepEqual(validate("some-package"), {valid: true})
+  t.deepEqual(validate("example.com"), {valid: true})
+  t.deepEqual(validate("under_score"), {valid: true})
+  t.deepEqual(validate("period.js"), {valid: true})
+  t.deepEqual(validate("123numeric"), {valid: true})
+  t.deepEqual(validate("crazy!"), {valid: true})
 
   // Scoped (npm 2+)
 
-  t.deepEqual(valid("@npm/thingy"), {valid: true})
-  t.deepEqual(valid("@npm-zors/money!time.js"), {valid: true})
+  t.deepEqual(validate("@npm/thingy"), {valid: true})
+  t.deepEqual(validate("@npm-zors/money!time.js"), {valid: true})
 
   // Invalid
 
-  t.deepEqual(valid(""), {
+  t.deepEqual(validate(""), {
     valid: false,
     errors: ["name length must be greater than zero"]})
 
-  t.deepEqual(valid(""), {
+  t.deepEqual(validate(""), {
     valid: false,
     errors: ["name length must be greater than zero"]})
 
-  t.deepEqual(valid(".start-with-period"), {
+  t.deepEqual(validate(".start-with-period"), {
     valid: false,
     errors: ["name cannot start with a period"]})
 
-  t.deepEqual(valid("_start-with-underscore"), {
+  t.deepEqual(validate("_start-with-underscore"), {
     valid: false,
     errors: ["name cannot start with an underscore"]})
 
-  t.deepEqual(valid("contain:colons"), {
+  t.deepEqual(validate("contain:colons"), {
     valid: false,
     errors: ["name can only contain URL-friendly characters"]})
 
-  t.deepEqual(valid("1234567890123456789012345678901234567890-more-than-fifty"), {
+  t.deepEqual(validate("1234567890123456789012345678901234567890-more-than-fifty"), {
     valid: false,
     errors: ["name cannot be longer than 50 characters"]})
 
-  t.deepEqual(valid(" leading-space"), {
+  t.deepEqual(validate(" leading-space"), {
     valid: false,
     errors: ["name cannot contain leading or trailing spaces", "name can only contain URL-friendly characters"]})
 
-  t.deepEqual(valid("trailing-space "), {
+  t.deepEqual(validate("trailing-space "), {
     valid: false,
     errors: ["name cannot contain leading or trailing spaces", "name can only contain URL-friendly characters"]})
 
-  t.deepEqual(valid("s/l/a/s/h/e/s"), {
+  t.deepEqual(validate("s/l/a/s/h/e/s"), {
     valid: false,
     errors: ["name can only contain URL-friendly characters"]})
 
-  t.deepEqual(valid("node_modules"), {
+  t.deepEqual(validate("node_modules"), {
     valid: false,
     errors: ["node_modules is a blacklisted name"]})
 
-  t.deepEqual(valid("favicon.ico"), {
+  t.deepEqual(validate("favicon.ico"), {
     valid: false,
     errors: ["favicon.ico is a blacklisted name"]})
 
   // Sketchy
 
-  t.deepEqual(valid("http"), {
+  t.deepEqual(validate("http"), {
     valid: true,
     warnings: ["http is a node core module name"]})
 
   // Legacy Mixed-Case
 
-  t.deepEqual(valid("CAPITAL-LETTERS", {allowMixedCase: true}), {valid: true})
+  t.deepEqual(validate("CAPITAL-LETTERS", {allowMixedCase: true}), {valid: true})
 
-  t.deepEqual(valid("CAPITAL-LETTERS"), {
+  t.deepEqual(validate("CAPITAL-LETTERS"), {
     valid: false,
     errors: ["name must be lowercase"]})
 
