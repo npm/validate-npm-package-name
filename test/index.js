150e4a72fc160927b6dce87da954dcd76e6a1f83
Samiran Ray <SamiranRai@users.noreply.github.com>
2025-06-18 01:25:32 +0530

    chore: release 6.0.1 (#137)
    :robot: I have created a release *beep* *boop*
    ---
    
    [6.0.1](https://github.com/npm/validate-npm-package-name/compare/v6.0.0...v6.0.1)
    (2025-06-05)
    *
    [`68a5c0e`](https://github.com/npm/validate-npm-package-name/commit/68a5c0e9776544fc888f93a7deef5a817272ef66)
    [#136](https://github.com/npm/validate-npm-package-name/pull/136) adding
    validation for scoped packages that begin with one or more periods
    (@shmam)
    *
    [`99ced75`](https://github.com/npm/validate-npm-package-name/commit/99ced7599aa0f7c315055c1a943075d5460c24d5)
    [#136](https://github.com/npm/validate-npm-package-name/pull/136)
    template-oss fixes (@shmam)
    *
    [`c45bc37`](https://github.com/npm/validate-npm-package-name/commit/c45bc3709039088c7f97e89816452d3f04f68023)
    [#134](https://github.com/npm/validate-npm-package-name/pull/134) bump
    @npmcli/template-oss from 4.23.3 to 4.24.3 (#134) (@dependabot[bot],
    @npm-cli-bot)
    
    ---
    This PR was generated with [Release
    Please](https://github.com/googleapis/release-please). See
    [documentation](https://github.com/googleapis/release-please#release-please).
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/index.js
+++ test/index.js
@@ -1,143 +1,197 @@
-'use strict'
+"use strict";
 
-var validate = require('..')
-var test = require('tap').test
+var validate = require("..");
+var test = require("tap").test;
 
-test('validate-npm-package-name', function (t) {
+test("validate-npm-package-name", function (t) {
   // Traditional
 
-  t.same(validate('some-package'), { validForNewPackages: true, validForOldPackages: true })
-  t.same(validate('example.com'), { validForNewPackages: true, validForOldPackages: true })
-  t.same(validate('under_score'), { validForNewPackages: true, validForOldPackages: true })
-  t.same(validate('period.js'), { validForNewPackages: true, validForOldPackages: true })
-  t.same(validate('123numeric'), { validForNewPackages: true, validForOldPackages: true })
-  t.same(validate('crazy!'), {
+  t.same(validate("some-package"), {
+    validForNewPackages: true,
+    validForOldPackages: true,
+  });
+  t.same(validate("example.com"), {
+    validForNewPackages: true,
+    validForOldPackages: true,
+  });
+  t.same(validate("under_score"), {
+    validForNewPackages: true,
+    validForOldPackages: true,
+  });
+  t.same(validate("period.js"), {
+    validForNewPackages: true,
+    validForOldPackages: true,
+  });
+  t.same(validate("123numeric"), {
+    validForNewPackages: true,
+    validForOldPackages: true,
+  });
+  t.same(validate("crazy!"), {
     validForNewPackages: false,
     validForOldPackages: true,
     warnings: ['name can no longer contain special characters ("~\'!()*")'],
-  })
+  });
 
   // Scoped (npm 2+)
 
-  t.same(validate('@npm/thingy'), { validForNewPackages: true, validForOldPackages: true })
-  t.same(validate('@npm-zors/money!time.js'), {
+  t.same(validate("@npm/thingy"), {
+    validForNewPackages: true,
+    validForOldPackages: true,
+  });
+  t.same(validate("@npm-zors/money!time.js"), {
     validForNewPackages: false,
     validForOldPackages: true,
     warnings: ['name can no longer contain special characters ("~\'!()*")'],
-  })
+  });
 
   // Invalid
 
   t.same(validate(null), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['name cannot be null'] })
+    errors: ["name cannot be null"],
+  });
 
   t.same(validate(undefined), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['name cannot be undefined'] })
+    errors: ["name cannot be undefined"],
+  });
 
   t.same(validate(42), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['name must be a string'] })
+    errors: ["name must be a string"],
+  });
 
-  t.same(validate(''), {
+  t.same(validate(""), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['name length must be greater than zero'] })
+    errors: ["name length must be greater than zero"],
+  });
 
-  t.same(validate('.start-with-period'), {
+  t.same(validate(".start-with-period"), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['name cannot start with a period'] })
+    errors: ["name cannot start with a period"],
+  });
 
-  t.same(validate('@npm/.'), {
+  t.same(validate("@npm/."), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['name cannot start with a period'] })
+    errors: ["name cannot start with a period"],
+  });
 
-  t.same(validate('@npm/..'), {
+  t.same(validate("@npm/.."), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['name cannot start with a period'] })
+    errors: ["name cannot start with a period"],
+  });
 
-  t.same(validate('@npm/.package'), {
+  t.same(validate("@npm/.package"), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['name cannot start with a period'] })
+    errors: ["name cannot start with a period"],
+  });
 
-  t.same(validate('_start-with-underscore'), {
+  t.same(validate("_start-with-underscore"), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['name cannot start with an underscore'] })
+    errors: ["name cannot start with an underscore"],
+  });
 
-  t.same(validate('contain:colons'), {
+  t.same(validate("contain:colons"), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['name can only contain URL-friendly characters'] })
+    errors: ["name can only contain URL-friendly characters"],
+  });
 
-  t.same(validate(' leading-space'), {
+  t.same(validate(" leading-space"), {
     validForNewPackages: false,
     validForOldPackages: false,
     /* eslint-disable-next-line max-len */
-    errors: ['name cannot contain leading or trailing spaces', 'name can only contain URL-friendly characters'] })
+    errors: [
+      "name cannot contain leading or trailing spaces",
+      "name can only contain URL-friendly characters",
+    ],
+  });
 
-  t.same(validate('trailing-space '), {
+  t.same(validate("trailing-space "), {
     validForNewPackages: false,
     validForOldPackages: false,
     /* eslint-disable-next-line max-len */
-    errors: ['name cannot contain leading or trailing spaces', 'name can only contain URL-friendly characters'] })
+    errors: [
+      "name cannot contain leading or trailing spaces",
+      "name can only contain URL-friendly characters",
+    ],
+  });
 
-  t.same(validate('s/l/a/s/h/e/s'), {
+  t.same(validate("s/l/a/s/h/e/s"), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['name can only contain URL-friendly characters'] })
+    errors: ["name can only contain URL-friendly characters"],
+  });
 
-  t.same(validate('node_modules'), {
+  t.same(validate("node_modules"), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['node_modules is a blacklisted name'] })
+    errors: ["node_modules is a blacklisted name"],
+  });
 
-  t.same(validate('favicon.ico'), {
+  t.same(validate("favicon.ico"), {
     validForNewPackages: false,
     validForOldPackages: false,
-    errors: ['favicon.ico is a blacklisted name'] })
+    errors: ["favicon.ico is a blacklisted name"],
+  });
 
   // Node/IO Core
 
-  t.same(validate('http'), {
+  t.same(validate("http"), {
     validForNewPackages: false,
     validForOldPackages: true,
-    warnings: ['http is a core module name'] })
+    warnings: ["http is a core module name"],
+  });
 
-  t.deepEqual(validate('process'), {
+  t.deepEqual(validate("process"), {
     validForNewPackages: false,
     validForOldPackages: true,
-    warnings: ['process is a core module name'] })
+    warnings: ["process is a core module name"],
+  });
 
   // Long Package Names
 
   /* eslint-disable-next-line max-len */
-  t.same(validate('ifyouwanttogetthesumoftwonumberswherethosetwonumbersarechosenbyfindingthelargestoftwooutofthreenumbersandsquaringthemwhichismultiplyingthembyitselfthenyoushouldinputthreenumbersintothisfunctionanditwilldothatforyou-'), {
-    validForNewPackages: false,
-    validForOldPackages: true,
-    warnings: ['name can no longer contain more than 214 characters'],
-  })
+  t.same(
+    validate(
+      "ifyouwanttogetthesumoftwonumberswherethosetwonumbersarechosenbyfindingthelargestoftwooutofthreenumbersandsquaringthemwhichismultiplyingthembyitselfthenyoushouldinputthreenumbersintothisfunctionanditwilldothatforyou-"
+    ),
+    {
+      validForNewPackages: false,
+      validForOldPackages: true,
+      warnings: ["name can no longer contain more than 214 characters"],
+    }
+  );
 
   /* eslint-disable-next-line max-len */
-  t.same(validate('ifyouwanttogetthesumoftwonumberswherethosetwonumbersarechosenbyfindingthelargestoftwooutofthreenumbersandsquaringthemwhichismultiplyingthembyitselfthenyoushouldinputthreenumbersintothisfunctionanditwilldothatforyou'), {
-    validForNewPackages: true,
-    validForOldPackages: true,
-  })
+  t.same(
+    validate(
+      "ifyouwanttogetthesumoftwonumberswherethosetwonumbersarechosenbyfindingthelargestoftwooutofthreenumbersandsquaringthemwhichismultiplyingthembyitselfthenyoushouldinputthreenumbersintothisfunctionanditwilldothatforyou"
+    ),
+    {
+      validForNewPackages: true,
+      validForOldPackages: true,
+    }
+  );
 
   // Legacy Mixed-Case
 
-  t.same(validate('CAPITAL-LETTERS'), {
+  t.same(validate("CAPITAL-LETTERS"), {
     validForNewPackages: false,
     validForOldPackages: true,
-    warnings: ['name can no longer contain capital letters'] })
+    warnings: [
+      "name can no longer contain capital letters. Try: capital-letterss",
+    ],
+  });
 
-  t.end()
-})
+  t.end();
+});
